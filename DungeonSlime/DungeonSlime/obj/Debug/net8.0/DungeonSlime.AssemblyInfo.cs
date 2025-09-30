e18cff930dd4d356243394786fbd1b267414bb9c
Julien Fougery <web@fougery.ovh>
2025-09-30 05:37:40 +0200

    .

--- DungeonSlime/DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
+++ DungeonSlime/DungeonSlime/obj/Debug/net8.0/DungeonSlime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c139edf9aa2880065e22797794c828c8caea38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5af066e1f0d05d00f92fc34fc2b5822e2b4048")]
 [assembly: System.Reflection.AssemblyProductAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DungeonSlime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
