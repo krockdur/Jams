623de678743f50ce95051deec37c7a1b9be212c3
Julien Fougery <web@fougery.ovh>
2025-10-27 20:27:05 +0100

    .

---
+++ Mega Byte Jam 2025/Defold/MegaByteJam2025/main/pipe_horizontal.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"pipe_horizontal\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/pipe_tilesource.tilesource\"\n"
+  "}\n"
+  ""
+}
