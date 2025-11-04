7d42a997158b8daaeaf68536cc4a3510f6dc45b1
Julien Fougery <web@fougery.ovh>
2025-11-04 05:29:50 +0100

    .

--- Mega Byte Jam 2025/Defold/MegaByteJam2025/main/scripts/pipe.go
+++ Mega Byte Jam 2025/Defold/MegaByteJam2025/main/scripts/pipe.go
@@ -1,7 +1,11 @@
+components {
+  id: "pipe"
+  component: "/main/scripts/pipe.script"
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"pipe_horizontal\"\n"
+  data: "default_animation: \"empty\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
