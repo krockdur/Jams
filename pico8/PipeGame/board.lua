5e19b0d83fc04a0fc08ffe3149ac23e3678be1a1
Julien Fougery <web@fougery.ovh>
2026-01-04 21:08:58 +0100

    .

--- pico8/PipeGame/board.lua
+++ pico8/PipeGame/board.lua
@@ -1,9 +1,7 @@
+nb_tile_x = 128 / 8
+nb_tile_y = 128 / 8
 
-
-nb_tile_x = 128/8
-nb_tile_y = 128/8
-
-board={}
+board = {}
 
 --board[j][i] = {type, wet}
 
@@ -11,14 +9,9 @@ board={}
 function are_pipes_left_and_rigth_connected(p1, p2) -- ex 17, 22
     local connected = false
     if p1 ~= 0 and p2 ~= 0 then
-        print("p1 " .. p1)
-        print("p2 " .. p2)
-        for k1, v1 in pairs(pipes[pipes[p1]].connected) do
-            for k2, v2 in pairs(pipes[pipes[p2]].connected) do
-                if v1 == "right" and v2 == "left" then
-                    connected = true
-                end
-            end
+        -- left and right
+        if pipes[pipes[p1]].connected[4] == 1 and pipes[pipes[p2]].connected[3] == 1 then
+            connected = true
         end
     end
     return connected
@@ -27,14 +20,10 @@ end
 function are_pipes_top_and_down_connected(p1, p2) -- ex 17, 22
     local connected = false
     if p1 ~= 0 and p2 ~= 0 then
-        print("p1 " .. p1)
-        print("p2 " .. p2)
-        for k1, v1 in pairs(pipes[pipes[p1]].connected) do
-            for k2, v2 in pairs(pipes[pipes[p2]].connected) do
-                if v1 == "bot" and v2 == "top" then
-                    connected = true
-                end
-            end
+        -- bot and top
+
+        if pipes[pipes[p1]].connected[2] == 1 and pipes[pipes[p2]].connected[1] == 1 then
+            connected = true
         end
     end
     return connected
@@ -45,38 +34,52 @@ function init_board()
 end
 
 function update_board()
-
     if global.update_board == true then
         local updating = true
-        while updating do
-            for j=0, 15 do
-                for i=0, 15 do
-                    if are_pipes_left_and_rigth_connected(mget(i, j), mget(i+1, j)) == true then
-                        updating = true
-                        if fget(mget(i, j)) == 2 or fget(mget(i+1, j)) == 2 then
-                            -- to wet
-                            mset(i, j, pipes[pipes[mget(i, j)]].tile_wet)
-                            mset(i+1, j, pipes[pipes[mget(i+1, j)]].tile_wet)
-                        end
-                    else
-                        updating = false
-                    end
-                    if are_pipes_top_and_down_connected(mget(i, j), mget(i, j+1)) == true then
-                        updating = true
-                        if fget(mget(i, j)) == 2 or fget(mget(i, j+1)) == 2 then
-                            -- to wet
-                            mset(i, j, pipes[pipes[mget(i, j)]].tile_wet)
-                            mset(i, j+1, pipes[pipes[mget(i, j+1)]].tile_wet)
-                        end
-                    else
-                        updating = false
-                    end
+
+        -- make all pipes dry
+        for j = 0, 15 do
+            for i = 0, 15 do
+                if fget(mget(i, j)) == 2 then
+                    mset(i, j, pipes[pipes[mget(i, j)]].tile_dry)
                 end
             end
         end
+        -- update wet pipes
+        while updating do
+
+            updating = false
+            for j = 0, 15 do
+                for i = 0, 15 do
+
+                    if are_pipes_left_and_rigth_connected(mget(i, j), mget(i + 1, j)) == true then
+                        if (fget(mget(i, j)) == 2 and fget(mget(i + 1, j)) == 1)  or (fget(mget(i, j)) == 1 and fget(mget(i + 1, j)) == 2) then
+                            updating = true
+                            -- to wet
+                            mset(i, j, pipes[pipes[mget(i, j)]].tile_wet)
+                            mset(i + 1, j, pipes[pipes[mget(i + 1, j)]].tile_wet)
+                        end
+                    end
+
+
+                    if are_pipes_top_and_down_connected(mget(i, j), mget(i, j + 1)) == true then
+
+                        if (fget(mget(i, j)) == 2 and fget(mget(i, j + 1)) == 1) or (fget(mget(i, j)) == 1 and fget(mget(i, j + 1)) == 2) then
+                            updating = true
+                            -- to wet
+                            printh("tile i j : " .. mget(i, j), "debug", true, true)
+                            printh("tile i j+1 : " .. mget(i, j+1), "debug",false, true)
+                            mset(i, j, pipes[pipes[mget(i, j)]].tile_wet)
+                            mset(i, j + 1, pipes[pipes[mget(i, j + 1)]].tile_wet)
+                        end
+                        
+                    end
+                end
+            end
+
+        end
         global.update_board = false
     end
-
 end
 
 function draw_board()
--- pico8/PipeGame/pipes.lua
+++ pico8/PipeGame/pipes.lua
@@ -21,65 +21,71 @@ pipes[21] = "corner_bot_right"
 pipes[24] = "corner_top_right"
 pipes[23] = "corner_top_right"
 
+-- connected : top, bot, left, right
+
 pipes["start_bot"]={}
-pipes["start_bot"]["tile"]=1
-pipes["start_bot"]["connected"]={"bot"}
+pipes["start_bot"]["tile_wet"]=1
+pipes["start_bot"]["tile_dry"]=1
+pipes["start_bot"]["connected"]={0, 1, 0, 0}
 
 pipes["start_top"]={}
-pipes["start_top"]["tile"]=2
-pipes["start_top"]["connected"]={"top"}
+pipes["start_top"]["tile_wet"]=2
+pipes["start_top"]["tile_dry"]=2
+pipes["start_top"]["connected"]={1, 0, 0, 0}
 
 pipes["start_right"]={}
-pipes["start_right"]["tile"]=3
-pipes["start_right"]["connected"]={"right"}
+pipes["start_right"]["tile_wet"]=3
+pipes["start_right"]["tile_dry"]=3
+pipes["start_right"]["connected"]={0, 0, 0, 1}
 
 pipes["start_left"]={}
-pipes["start_left"]["tile"]=4
-pipes["start_left"]["connected"]={"left"}
+pipes["start_left"]["tile_wet"]=4
+pipes["start_left"]["tile_dry"]=4
+pipes["start_left"]["connected"]={0, 0, 1, 0}
 
 pipes["hori"]={}
 pipes["hori"]["tile_dry"]=6
 pipes["hori"]["tile_wet"]=5
-pipes["hori"]["connected"]={"left", "right"}
+pipes["hori"]["connected"]={0, 0, 1, 1}
 
 
 pipes["verti"]={}
 pipes["verti"]["tile_dry"]=8
 pipes["verti"]["tile_wet"]=7
-pipes["verti"]["connected"]={"top", "bot"}
+pipes["verti"]["connected"]={1, 1, 0, 0}
 
 
 pipes["full_cross"]={}
 pipes["full_cross"]["tile_dry"]=10
 pipes["full_cross"]["tile_wet"]=9
-pipes["full_cross"]["connected"]={"top", "bot", "left", "right"}
+pipes["full_cross"]["connected"]={1, 1, 1, 1}
 
 
 pipes["half_cross_hori_wet"]={}
 pipes["half_cross_hori_wet"]["tile"]=25
-pipes["half_cross_hori_wet"]["connected"]={"top", "bot", "left", "right"}
+pipes["half_cross_hori_wet"]["connected"]={1, 1, 1, 1}
 
 pipes["half_cross_verti_wet"]={}
 pipes["half_cross_verti_wet"]["tile"]=26
-pipes["half_cross_verti_wet"]["connected"]={"top", "bot", "left", "right"}
+pipes["half_cross_verti_wet"]["connected"]={1, 1, 1, 1}
 
 pipes["corner_bot_left"]={}
 pipes["corner_bot_left"]["tile_dry"]=18
 pipes["corner_bot_left"]["tile_wet"]=17
-pipes["corner_bot_left"]["connected"]={"top", "right"}
+pipes["corner_bot_left"]["connected"]={1, 0, 0, 1}
 
 
 pipes["corner_top_left"]={}
 pipes["corner_top_left"]["tile_dry"]=20
 pipes["corner_top_left"]["tile_wet"]=19
-pipes["corner_top_left"]["connected"]={"bot", "right"}
+pipes["corner_top_left"]["connected"]={0, 1, 0, 1}
 
 pipes["corner_bot_right"]={}
 pipes["corner_bot_right"]["tile_dry"]=21
 pipes["corner_bot_right"]["tile_wet"]=22
-pipes["corner_bot_right"]["connected"]={"top", "left"}
+pipes["corner_bot_right"]["connected"]={1, 0, 1, 0}
 
 pipes["corner_top_right"]={}
 pipes["corner_top_right"]["tile_dry"]=23
 pipes["corner_top_right"]["tile_wet"]=24
-pipes["corner_top_right"]["connected"]={"bot", "left"}
+pipes["corner_top_right"]["connected"]={0, 1, 1, 0}
